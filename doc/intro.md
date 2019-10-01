68ecb74854f662459b048550b6ee789f5ae2744c
Lei Wang <tendant@gmail.com>
2019-10-01 13:00:15 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ring-reload
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ring_reload/core_test.clj
@@ -0,0 +1,7 @@
+(ns ring-reload.core-test
+  (:require [clojure.test :refer :all]
+            [ring-reload.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
