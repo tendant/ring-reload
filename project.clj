361e4ebe4257200121b009bca323f5dca714889d
Lei Wang <tendant@gmail.com>
2019-10-01 13:04:56 -0700

    copy file ring.middleware.reload

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject ring-reload "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject tendant/ring-reload "0.1.0-SNAPSHOT"
+  :description "Ring reload middelware, use it before new release of ring (1.7.1 as of now)"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns ring-reload.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ns-tracker "0.3.1"]])
